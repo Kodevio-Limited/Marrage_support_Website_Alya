c80b4d884a6b3ce9f776c1e842aa5843fb34ce56
samiul2102013 <samiulhasan0186@gmail.com>
2026-08-20 11:18:48 +0600

    Fix standalone crash: include @swc/helpers/esm via outputFileTracingIncludes

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  outputFileTracingIncludes: {
+    '*': ['./node_modules/@swc/helpers/esm/**/*'],
+  },
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1280, 1920, 2048, 3840],
     dangerouslyAllowLocalIP: true,
